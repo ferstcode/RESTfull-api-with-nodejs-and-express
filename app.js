a8b73de9f31abe082f16668845990f7032a26fb1
ferstcode <katheryne.arancib.12@sansano.usm.cl>
2020-01-27 12:42:25 -0300

    using express.Router

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const bookRouter = express.Router();  //.Router= clase para crear manejadores de rutas
+
 const port = process.env.PORT || 3000;
 
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = { hello : 'This is my API'};
+    res.json(response);
+  });
+app.use('/api', bookRouter);          
+
 app.get('/', (req, res) => {
-  res.send('Welome to my API');
+  res.send('Welome to my API!!!');
 });
 
 app.listen(port, () => {
