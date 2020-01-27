a50b2d131d4f2176affeb316bd9c11fec1dcb6c1
ferstcode <katheryne.arancib.12@sansano.usm.cl>
2020-01-27 13:33:53 -0300

    connection of mongodb and adding bd of books

---
+++ booksJson.js
@@ -0,0 +1,56 @@
+db.books.insert([
+    {
+        title: 'Te espero en la próxima estación',
+        genre: 'Novela urbana',
+        author: 'Fernando Pardo Ruiz',
+        read: false
+    },
+    {
+        title: 'La mujer del otro',
+        genre: 'Novela urbana',
+        author: 'José Navarro Ballesteros',
+        read: false
+    },
+    {
+        title: 'Lo inefable del amor',
+        genre: 'Narrativa urbana',
+        author: 'Sara Carballal',
+        read: false
+    },
+    {
+        title: 'Conatus',
+        genre: 'Prosa poética',
+        author: 'Belén Blesa Aledo',
+        read: false
+    },
+    {
+        title: 'El exilio del alma - Sobre María Zambrano',
+        genre: 'Humanismo',
+        author: 'José Ignacio Eguizábal',
+        read: false
+    },
+    {
+        title: '¿Dónde están los poetas cuando se les necesita?',
+        genre: 'Narrativa',
+        author: 'Iván Robledo',
+        read: false
+    },
+    {
+        title: 'Las voces amarillas',
+        genre: 'Novela negra',
+        author: 'Luis Veres',
+        read: false
+    },
+    {
+        title: 'SUB SUA',
+        genre: 'Novela negra',
+        author: 'Carla T. Kohlberg',
+        read: false
+    },
+    {
+        title: 'Los que miran las estrellas',
+        genre: 'Narrativa',
+        author: 'Alba Oliva',
+        read: false
+    },
+]);
